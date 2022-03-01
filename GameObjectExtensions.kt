edcf280512a13bac7fd9a280f2742f1e17102ee7
marceloomg13 <73548693+marceloomg13@users.noreply.github.com>
2022-03-01 22:03:03 +0100

    Delete GameObjectExtensions.kt

--- GameObjectExtensions.kt
+++
@@ -1,5 +0,0 @@
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-
-val GameObject.xOffset: Dp get() = position.x.dp - (size.dp / 2)
-val GameObject.yOffset: Dp get() = position.y.dp - (size.dp / 2)
